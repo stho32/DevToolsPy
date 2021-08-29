49477ee4728c5610a2344fdf26ee4ed13a2f2ac7
stho32 <stho32github@gmail.com>
2021-08-29 18:41:33 +0200

    added python requirements to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/bash
 
+## we need pip for certain operations
+sudo apt install python3-pip
+
 ## clone the repo
 git clone https://github.com/stho32/DevToolsPy
 
 cd DevToolsPy
 
+## we need that lib for table format
+pip3 install -r requirements.txt
+
 ## copy the export line to bashrc for global use!
 echo "export PATH=\"\$PATH:$(pwd)/source\"" >> ~/.bashrc
 
 cd ..
-
-## we need pip for certain operations
-sudo apt install python3-pip
