3954722e729d9406e599a016c1cb5c46590fa0b9
naseif <d2mds21@gmail.com>
2021-08-29 18:32:43 +0200

    Used prettyTable to improve the output of the script! :)

--- source/git-pull-all.py
+++ source/git-pull-all.py
@@ -1,12 +1,21 @@
 #!/bin/python3
 import os
+import subprocess
+from prettytable import PrettyTable
+
+t = PrettyTable(['Repo', 'Status'])
 
 for f in os.listdir("."):
     if not os.path.isfile(f):
-        print("  - entering " + f) 
         os.chdir(f)
-        print("  - pulling stuff")
-        os.system("git pull")
-        print("  - exiting " + f)
+        proc = subprocess.Popen(
+            ["git", "pull"], stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
+        if "Updating" in out.decode("utf-8"):
+            out = "Updated"
+        else:
+            out = "Already up to date!"
+        t.add_row([f, out])
         os.chdir("..")
 
+print(t)
