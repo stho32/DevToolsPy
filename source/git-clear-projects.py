b4597d9898f3c2dd20c15e19122c02c848a7be1b
stho32 <stho32github@gmail.com>
2021-12-26 02:58:53 +0100

    tool to clean project directory from stuff that doesn't need to be cloned

---
+++ source/git-clear-projects.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+import os
+import subprocess
+import shutil
+from prettytable import PrettyTable
+
+
+def simplify_message(message):
+    if "unverändert" in message:
+        return "CLEAR"
+    return "unclean"
+
+
+def git_status(doIt):
+    t = PrettyTable(['Nr.', 'Repo', 'Status', 'Action'])
+
+    number = 0
+    directories = []
+
+    for f in os.listdir("."):
+        if not os.path.isfile(f):
+            directories.append(f)
+
+    directories = sorted(directories)
+
+    for directory in directories:
+        number += 1
+        os.chdir(directory)
+        proc = subprocess.Popen(["git", "status"], stdout=subprocess.PIPE)
+        (out, err) = proc.communicate()
+        out = simplify_message(out.decode("utf-8"))
+        action = ""
+        fulldir = os.path.abspath(os.getcwd())
+        os.chdir("..")
+        
+        if out == "CLEAR":
+            if not directory.lower().startswith("devtools"):
+                action = "Remove"
+                if doIt:
+                    print(fulldir)
+                    shutil.rmtree(fulldir)
+
+        t.add_row([number, directory, out, action])
+
+    t.align = "l"
+    if not doIt:
+        print(t)
+
+
+if __name__ == "__main__":
+    git_status(False)
+    performActionsInput = input("Perform actions (y/N)?")
+    if performActionsInput == "y":
+        git_status(True)
